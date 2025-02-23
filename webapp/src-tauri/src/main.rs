0e70d3226b3a25d3618edf9eeb8b2da6f3a1b165
Michael Noronha <michaeltnoronha@gmail.com>
2025-02-22 17:11:35 -0800

    Revert "ckpt: greet from rust"
    This reverts commit d31e8ac0718ed7cc5cca60837505ae84e7007268.

--- webapp/src-tauri/src/main.rs
+++ webapp/src-tauri/src/main.rs
@@ -1,16 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::command;
-
-#[command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}!", name)
-}
-
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running Tauri application");
+  app_lib::run();
 }
