d4afadf58e00b4a3c411b7a73d78338fe03d5d85
Michael Noronha <michaeltnoronha@gmail.com>
2025-02-23 17:20:49 -0800

    cleanup

--- app/src-tauri/src/debugger/mod.rs
+++ app/src-tauri/src/debugger/mod.rs
@@ -97,39 +97,8 @@ impl DebugManager {
             .emit("debug-status", DebugStatus::Running)
             .map_err(|e| e.to_string())?;
 
-        // Spawn a thread to monitor the process
-        // let app_handle_clone = app_handle.clone();
-        // let process = self.process.lock().unwrap().as_mut().unwrap().id();
-        // std::thread::spawn(move || {
-        //     loop {
-        //         std::thread::sleep(std::time::Duration::from_millis(100));
-
-        //         let status = Command::new("ps")
-        //             .arg("-p")
-        //             .arg(process.to_string())
-        //             .stdout(std::process::Stdio::null())
-        //             .status();
-
-        //         match status {
-        //             Ok(exit_status) if !exit_status.success() => {
-        //                 // Process has terminated
-        //                 println!("Debugpy process terminated"); // Debug log
-        //                 let _ = app_handle_clone.emit("debug-status", DebugStatus::Terminated);
-        //                 break;
-        //             }
-        //             Err(_) => {
-        //                 // Process not found
-        //                 println!("Debugpy process not found"); // Debug log
-        //                 let _ = app_handle_clone.emit("debug-status", DebugStatus::Terminated);
-        //                 break;
-        //             }
-        //             _ => {
-        //                 println!("Debugpy seems to be running");
-        //             }
-        //         }
-        //     }
-        // });
-
+        // TODO make this more robust
+        // Wait to give debugpy time to start up
         std::thread::sleep(std::time::Duration::from_secs(2));
 
         // Spawn an asynchronous listener to receive messages from debugpy.
