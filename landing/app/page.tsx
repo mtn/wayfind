564a646ddacd592c4fafb180a08a760898e54ee5
Michael Noronha <michaeltnoronha@gmail.com>
2025-03-16 10:44:22 -0700

    cloudflare page build test

--- landing/app/page.tsx
+++ landing/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="h-screen w-screen flex flex-col items-center justify-center bg-white text-black">
       <div className="text-center">
-        <h1 className="text-6xl font-mono font-bold">wayfindd</h1>
+        <h1 className="text-6xl font-mono font-bold">wayfinddd</h1>
       </div>
     </main>
   );
